8026a1313fae6603fd95babe0654495085e3f05a
Cate Uselton <cathode.use@gmail.com>
2014-11-08 16:27:19 -0800

    log in works

---
+++ cateu/cooks/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in!"
+
+    else
+      flash.now.alert = "Email or password is invalid"
+      render :new
+    end
+  end
+
+  def destroy #creates logout function
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged Out!"
+  end
+
+end
